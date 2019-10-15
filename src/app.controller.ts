19af07eafa88513231ca3854853afd33f81f9a73
Jose Selesan <jselesan@gmail.com>
2019-10-15 09:29:57 -0300

    First commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AppService } from './app.service';
+import { IsNumber, IsBoolean, IsNumberString, IsBooleanString } from 'class-validator';
+
+class TestDto {
+  @IsNumber()
+  field1: number;
+}
+
+// tslint:disable-next-line:max-classes-per-file
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @UsePipes( new ValidationPipe( { transform: true, whitelist: true }))
+  getHello(@Query() testDto: TestDto): TestDto {
+    return testDto;
+  }
+}
